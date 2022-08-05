90ecf12a73df8675adb32dfec77701e3b139a300
Zikora Ogbuagu <robbie89339@gmail.com>
2022-08-05 22:20:07 +0100

    feat: Moved generator process from timer callback to `_PhysicsProcess`

--- demo/AlchemyDemo.cs
+++ demo/AlchemyDemo.cs
@@ -3,38 +3,35 @@ using System;
 
 public class AlchemyDemo : Control
 {
+	private int panelCount;
 	private int currentSeedOffset;
 	private int displayPanelIndex = 0;
-
-	private Timer generatorTimer;
 	private GridContainer gridContainer;
 
+	private bool isGenerating;
 	private Label headerLabel;
 	private SidePanel sidePanel;
 	private Alchemist circleGenerator;
 	private SymbolDisplay[] displayPanels;
 
 	public override void _Ready()
-    {
-        gridContainer = GetNode<GridContainer>
-        ("BGPanel/MainSectionContainer/MainSection/GridContainer");
+	{
+		gridContainer = GetNode<GridContainer>
+		("BGPanel/MainSectionContainer/MainSection/GridContainer");
 		headerLabel = GetNode<Label>
 		("BGPanel/MainSectionContainer/MainSection/HeaderPanel/Label");
-        generatorTimer = GetNode<Timer>("GeneratorTimer");
 		sidePanel = GetNode<SidePanel>("BGPanel/SidePanel");
 
-        AssignSeedOffset();
-        generatorTimer.Connect("timeout", this,
-            nameof(_OnGeneratorTimerTimeout));
+		AssignSeedOffset();
+		Color backgroundColor = new Color
+		(0.141176f, 0.141176f, 0.141176f);
+		circleGenerator = new Alchemist
+		(Colors.WhiteSmoke, backgroundColor, 4, 256);
 
-        Color backgroundColor = new Color
-        (0.141176f, 0.141176f, 0.141176f);
-        circleGenerator = new Alchemist
-        (Colors.WhiteSmoke, backgroundColor, 4, 256);
-
-        InitializePanelArray();
-        generatorTimer.Start();
-    }
+		InitializePanelArray();
+		// Start generating
+		isGenerating = true;
+	}
 
 	private int GetSeedOffset()
 	{
@@ -43,18 +40,18 @@ public class AlchemyDemo : Control
 		((float) GD.RandRange(0.0, 500.0));
 	}
 
-    private void AssignSeedOffset() => currentSeedOffset = GetSeedOffset();
+	private void AssignSeedOffset() => currentSeedOffset = GetSeedOffset();
 
-    private void InitializePanelArray()
-    {
-        int panelCount = gridContainer.GetChildCount();
-        displayPanels = new SymbolDisplay[panelCount];
-        for (int i = 0; i < panelCount; i++)
-        {
-            displayPanels[i] = gridContainer
-            .GetChild<SymbolDisplay>(i);
-        }
-    }
+	private void InitializePanelArray()
+	{
+		panelCount = gridContainer.GetChildCount();
+		displayPanels = new SymbolDisplay[panelCount];
+		for (int i = 0; i < panelCount; i++)
+		{
+			displayPanels[i] = gridContainer
+			.GetChild<SymbolDisplay>(i);
+		}
+	}
 
 	public ImageTexture GenerateCircle(int seedValue)
 	{
@@ -64,35 +61,37 @@ public class AlchemyDemo : Control
 		return texture;
 	}
 
-    private void UpdateLoadingInfo(int currentIndex)
-    {
-		int panelCount = gridContainer.GetChildCount();
+	private void UpdateLoadingInfo(int currentIndex)
+	{
 		if (currentIndex != panelCount)
 		{
 			headerLabel.Text = "Symbols Generated " + 
 			$"({currentIndex} / {panelCount})";
 		}
 		else headerLabel.Text = "Complete :)";
-    }
-
-    public void _OnRegenerateButtonPressed()
-	{
-		if (generatorTimer.IsStopped()) generatorTimer.Start();
 	}
 
-	public void _OnGeneratorTimerTimeout()
+	public void _OnRegenerateButtonPressed()
 	{
-		SymbolDisplay displayPanel = displayPanels[displayPanelIndex];
-		displayPanel.seedValue = displayPanelIndex + currentSeedOffset;
-		displayPanel.circleTexture = GenerateCircle(displayPanelIndex + currentSeedOffset);
-		UpdateLoadingInfo(++displayPanelIndex);
-		
-		// All panels have been assigned to, clean up
-		if (displayPanelIndex == displayPanels.Length)
+		if (!isGenerating) isGenerating = true;
+	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		if (isGenerating)
 		{
-			AssignSeedOffset();
-			displayPanelIndex = 0;
-			generatorTimer.Stop();
+			SymbolDisplay displayPanel = displayPanels[displayPanelIndex];
+			displayPanel.seedValue = displayPanelIndex + currentSeedOffset;
+			displayPanel.circleTexture = GenerateCircle(displayPanelIndex + currentSeedOffset);
+			UpdateLoadingInfo(++displayPanelIndex);
+			
+			// All panels have been assigned to, clean up
+			if (displayPanelIndex == displayPanels.Length)
+			{
+				AssignSeedOffset();
+				displayPanelIndex = 0;
+				isGenerating = false;
+			}
 		}
 	}
 }
