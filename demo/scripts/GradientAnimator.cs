df67caf079e12226b1de154e9c7017bb16e55b84
Zikora Ogbuagu <robbie89339@gmail.com>
2022-08-05 22:18:45 +0100

    refactor: Indentation from spaces -> tabs

--- demo/scripts/GradientAnimator.cs
+++ demo/scripts/GradientAnimator.cs
@@ -3,73 +3,73 @@ using System;
 
 public class GradientAnimator : Panel
 {
-    private Gradient gradient;
-    private GradientTexture gradientTexture;
-    private StyleBoxTexture gradientStyleBox;
+	private Gradient gradient;
+	private GradientTexture gradientTexture;
+	private StyleBoxTexture gradientStyleBox;
 
-    // Animation Properties
-    private Vector2 clockHandA;
-    private Vector2 clockHandB;
-    
-    private Color colorA;
-    private Color colorB;
-    private float timeScale = 0.60f;
-    private float stepValue = 0.25f;
-    private OpenSimplexNoise noise;
+	// Animation Properties
+	private Vector2 clockHandA;
+	private Vector2 clockHandB;
+	
+	private Color colorA;
+	private Color colorB;
+	private float timeScale = 0.60f;
+	private float stepValue = 0.25f;
+	private OpenSimplexNoise noise;
 
-    public override void _Ready()
-    {
-        gradientStyleBox = (StyleBoxTexture) Get("custom_styles/panel");
-        gradientTexture  = (GradientTexture) gradientStyleBox.Texture;
-        gradient = gradientTexture.Gradient;
+	public override void _Ready()
+	{
+		gradientStyleBox = (StyleBoxTexture) Get("custom_styles/panel");
+		gradientTexture  = (GradientTexture) gradientStyleBox.Texture;
+		gradient = gradientTexture.Gradient;
 
-        colorA = gradient.Colors[0];
-        colorB = gradient.Colors[1];
-        
-        clockHandA = Vector2.Up;
-        clockHandB = Vector2.Down;
-        // "Clock Hands" will remain orthogonal to each other
-        
-        noise = new OpenSimplexNoise();
-        noise.Seed = (int) GD.Randi();
-        noise.Octaves = 3;
-        noise.Period = 20.0f; 
-        noise.Persistence = 0.8f;
-    }
+		colorA = gradient.Colors[0];
+		colorB = gradient.Colors[1];
+		
+		clockHandA = Vector2.Up;
+		clockHandB = Vector2.Down;
+		// "Clock Hands" will remain orthogonal to each other
+		
+		noise = new OpenSimplexNoise();
+		noise.Seed = (int) GD.Randi();
+		noise.Octaves = 3;
+		noise.Period = 20.0f; 
+		noise.Persistence = 0.8f;
+	}
 
-    public override void _PhysicsProcess(float delta)
-    {
-        if (gradient != null) AnimateGradient(delta);
-    }
+	public override void _PhysicsProcess(float delta)
+	{
+		if (gradient != null) AnimateGradient(delta);
+	}
 
-    /// <summary>
-    ///     Custom gradient animation algorithm.
-    ///     It works best with perlin noise but alas...
-    ///</summary>
-    public void AnimateGradient(float delta)
-    {
-        clockHandA = clockHandA.Rotated(delta * timeScale);
-        clockHandB = clockHandB.Rotated(delta * timeScale);
-        var colorAFactor = Wrapf(noise.GetNoise2dv(clockHandA), 0.1f, 1.0f);
-        var colorBFactor = Wrapf(noise.GetNoise2dv(clockHandB), 0.1f, 1.0f);
-        var colorAHue = colorA.h + (stepValue * timeScale * noise.GetNoise1d(delta));
-        var colorBHue = colorB.h + (stepValue * timeScale * noise.GetNoise1d(delta));
+	/// <summary>
+	///     Custom gradient animation algorithm.
+	///     It works best with perlin noise but alas...
+	///</summary>
+	public void AnimateGradient(float delta)
+	{
+		clockHandA = clockHandA.Rotated(delta * timeScale);
+		clockHandB = clockHandB.Rotated(delta * timeScale);
+		var colorAFactor = Wrapf(noise.GetNoise2dv(clockHandA), 0.1f, 1.0f);
+		var colorBFactor = Wrapf(noise.GetNoise2dv(clockHandB), 0.1f, 1.0f);
+		var colorAHue = colorA.h + (stepValue * timeScale * noise.GetNoise1d(delta));
+		var colorBHue = colorB.h + (stepValue * timeScale * noise.GetNoise1d(delta));
 
-        colorA = Color.FromHsv(Mathf.Lerp
-            (colorA.h, colorAHue, colorAFactor),
-                            colorA.s, colorA.v);
+		colorA = Color.FromHsv(Mathf.Lerp
+			(colorA.h, colorAHue, colorAFactor),
+							colorA.s, colorA.v);
 
-        colorB = Color.FromHsv(Mathf.Lerp
-            (colorB.h, colorBHue, colorBFactor),
-                            colorB.s, colorB.v);
-        
-        gradient.Colors = new Color[] { colorA, colorB };
-    }
+		colorB = Color.FromHsv(Mathf.Lerp
+			(colorB.h, colorBHue, colorBFactor),
+							colorB.s, colorB.v);
+		
+		gradient.Colors = new Color[] { colorA, colorB };
+	}
 
-    /// <summary>Float alternative for Mathf.Wrap()</summary>
-    public float Wrapf(float val, float min = 0.0f, float max = 1.0f)
-    {
-        if ((val < min) || (val > max)) return min;
-        return val;
-    }
+	/// <summary>Float alternative for Mathf.Wrap()</summary>
+	public float Wrapf(float val, float min = 0.0f, float max = 1.0f)
+	{
+		if ((val < min) || (val > max)) return min;
+		return val;
+	}
 }
--- demo/scripts/SidePanel.cs
+++ demo/scripts/SidePanel.cs
@@ -3,53 +3,53 @@ using System;
 
 public class SidePanel : Panel
 {
-    private Label seedLabel;
-    private Button generateButton;
-    private Button downloadButton;
-    private TextureRect symbolPreview;
-    private Alchemist circleGenerator;
-    private AlchemyDemo mainWindow;
+	private Label seedLabel;
+	private Button generateButton;
+	private Button downloadButton;
+	private TextureRect symbolPreview;
+	private Alchemist circleGenerator;
+	private AlchemyDemo mainWindow;
 
-    public override void _Ready()
-    {
-        seedLabel = GetNode<Label>
-        ("MarginContainer/VBoxContainer/SeedLabel");
-        generateButton = GetNode<Button>
-        ("MarginContainer/VBoxContainer/RGButtonContainer/Button");
-        downloadButton = GetNode<Button>
-        ("MarginContainer/VBoxContainer/DLButtonContainer/Button");
-        symbolPreview = GetNode<TextureRect>
-        ("MarginContainer/VBoxContainer/PreviewContainer/TextureRect");
-        mainWindow = GetNode<AlchemyDemo>("../..");
-        circleGenerator = new Alchemist
-            ( Colors.WhiteSmoke, new Color
-                (0.141176f, 0.141176f, 0.141176f),
-            4, 256);
-        _OnSymbolSelected(0, null);
-    }
+	public override void _Ready()
+	{
+		seedLabel = GetNode<Label>
+		("MarginContainer/VBoxContainer/SeedLabel");
+		generateButton = GetNode<Button>
+		("MarginContainer/VBoxContainer/RGButtonContainer/Button");
+		downloadButton = GetNode<Button>
+		("MarginContainer/VBoxContainer/DLButtonContainer/Button");
+		symbolPreview = GetNode<TextureRect>
+		("MarginContainer/VBoxContainer/PreviewContainer/TextureRect");
+		mainWindow = GetNode<AlchemyDemo>("../..");
+		circleGenerator = new Alchemist
+			( Colors.WhiteSmoke, new Color
+				(0.141176f, 0.141176f, 0.141176f),
+			4, 256);
+		_OnSymbolSelected(0, null);
+	}
 
-    public void _OnSymbolSelected(int seedValue, Texture texture)
-    {
-        int currentSeedValue = int.Parse
-        (seedLabel.Text.Replace("#", ""));
+	public void _OnSymbolSelected(int seedValue, Texture texture)
+	{
+		int currentSeedValue = int.Parse
+		(seedLabel.Text.Replace("#", ""));
 
-        if ((seedValue != currentSeedValue) && (texture != null)) 
-        {
-            symbolPreview.Texture = texture;
-            seedLabel.Text = $"#{seedValue}";
-        }
-    }
+		if ((seedValue != currentSeedValue) && (texture != null)) 
+		{
+			symbolPreview.Texture = texture;
+			seedLabel.Text = $"#{seedValue}";
+		}
+	}
 
-    public void _OnDownloadButtonPressed()
-    {
-        GD.Print("Download Begins");
-    }
+	public void _OnDownloadButtonPressed()
+	{
+		GD.Print("Download Begins");
+	}
 
-    public void _OnRegenerateButtonPressed()
-    {
-        // symbolPreview.Texture.Dispose();
-        
-        symbolPreview.Texture = null;
-        mainWindow._OnRegenerateButtonPressed();
-    }
+	public void _OnRegenerateButtonPressed()
+	{
+		// symbolPreview.Texture.Dispose();
+		
+		symbolPreview.Texture = null;
+		mainWindow._OnRegenerateButtonPressed();
+	}
 }
